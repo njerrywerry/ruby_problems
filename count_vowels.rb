281af5656bb5ccf847217c02bae6eb8ad4ec19d1
Sylvia <sylviakieha@gmail.com>
2015-08-11 19:14:34 +0300

    count vowels solved

---
+++ count_vowels.rb
@@ -0,0 +1,14 @@
+def count_vowels(string)
+
+arr = string.split("").keep_if do |x|
+	x =~ /[aeiou]/
+end
+return arr.length
+end
+
+puts count_vowels("mamamia")
+
+puts('count_vowels("abcd") == 1: ' + (count_vowels('abcd') == 1).to_s)
+puts('count_vowels("color") == 2: ' + (count_vowels('color') == 2).to_s)
+puts('count_vowels("colour") == 3: ' + (count_vowels('colour') == 3).to_s)
+puts('count_vowels("cecilia") == 4: ' + (count_vowels('cecilia') == 4).to_s)
