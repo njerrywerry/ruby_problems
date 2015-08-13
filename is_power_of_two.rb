eb0218216f29668fac551435cfcd743f6b1d86f3
Sylvia <sylviakieha@gmail.com>
2015-08-13 11:05:23 +0300

    is power of two solved

---
+++ is_power_of_two.rb
@@ -0,0 +1,18 @@
+def is_power_of_two?(num)
+
+if num == 0
+	return false
+elsif (num & (num - 1)) == 0
+	return true
+else
+	return false
+end
+end
+
+puts is_power_of_two?(4096)
+
+puts('is_power_of_two?(1) == true: ' + (is_power_of_two?(1) == true).to_s)
+puts('is_power_of_two?(16) == true: ' + (is_power_of_two?(16) == true).to_s)
+puts('is_power_of_two?(64) == true: ' + (is_power_of_two?(64) == true).to_s)
+puts('is_power_of_two?(78) == false: ' + (is_power_of_two?(78) == false).to_s)
+puts('is_power_of_two?(0) == false: ' + (is_power_of_two?(0) == false).to_s)
